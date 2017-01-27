903947685a89dfd56594a1df8bd26fbb3f5a2ee9
adabotcon <adabotcon@outlook.com>
2017-01-27 12:32:29 -0700

    Added query to Wordnik

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-var Wordnik_BASE_URL = '';
+var Wordnik_BASE_URL = 'http://api.wordnik.com:80/v4/words.json/randomWord';
 var Uiname_BASE_URL = '';
-var wordnik_key = '3c1825dd8857abc17e0060ff479098d6269c21519abe76465';
 
 function getDataFromWordnik(wordType, callback){
+	var query = {
+		hasDictionaryDef = false,
+		includePartOfSpeech = wordType,
+		minCorpusCount = 0,
+		maxCorpusCount = -1,
+		minDictionaryCount = 1,
+		maxDictionaryCount = -1,
+		minLength = 4,
+		maxLength = -1,
+		api_key = '3c1825dd8857abc17e0060ff479098d6269c21519abe76465'
+	}
 
+	$.getJSON(Wordnik_BASE_URL, query, callback);
 }
 
 function getDataFromUiname(gender, region, callback){
