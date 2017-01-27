6ed6eaddf487dca5acc488169c1d0fe5b8bc99e5
adabotcon <adabotcon@outlook.com>
2017-01-27 12:18:12 -0700

    Added basic HTML including Header and scripts. Added basic functions to JS

---
+++ app.js
@@ -0,0 +1,15 @@
+var Wordnik_BASE_URL = '';
+var Uiname_BASE_URL = '';
+
+function getDataFromWordnik(wordType, callback){
+
+}
+
+function getDataFromUiname(gender, region, callback){
+	
+}
+
+
+$(function(){
+	console.log('Hello World');
+})
