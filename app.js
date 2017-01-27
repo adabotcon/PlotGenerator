515f321d53d172171cdbbd9acfaf9836c6e1c66d
adabotcon <adabotcon@outlook.com>
2017-01-27 12:23:20 -0700

    Added wordnik key

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var Wordnik_BASE_URL = '';
 var Uiname_BASE_URL = '';
+var wordnik_key = '3c1825dd8857abc17e0060ff479098d6269c21519abe76465';
 
 function getDataFromWordnik(wordType, callback){
 
 }
 
 function getDataFromUiname(gender, region, callback){
-	
+
 }
 
 
