5875d28ff5757d81cb60b17266dcafd6e5ae759c
adabotcon <adabotcon@outlook.com>
2017-01-27 14:52:15 -0700

    Deleted Hello World

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var Wordnik_BASE_URL = 'http://api.wordnik.com:80/v4/words.json/randomWord';
-var Uiname_BASE_URL = '';
+var Uinames_BASE_URL = 'http://uinames.com/api/';
 
 function getDataFromWordnik(wordType, callback){
 	var query = {
@@ -12,21 +12,31 @@ function getDataFromWordnik(wordType, callback){
 		minLength: 4,
 		maxLength: -1,
 		api_key: '3c1825dd8857abc17e0060ff479098d6269c21519abe76465'
-	}
+	};
 
 	$.getJSON(Wordnik_BASE_URL, query, callback);
 }
 
-function getDataFromUiname(gender, region, callback){
+function getDataFromUinames(gender, region, callback){
+	var query = {
+		region: region,
+		gender: gender,
+		amount: 1
+	};
 
+	$.getJSON(Uinames_BASE_URL, query, callback);
 }
 
 function displayWordnikData(data){
 	console.log(data);
 }
 
+function displayUinamesData(data){
+	console.log(data);
+}
+
 
 $(function(){
 	getDataFromWordnik('verb', displayWordnikData);
-	console.log('Hello World');
+	getDataFromUinames('male', 'United States', displayUinamesData);
 })
