da910b4cb9a995a085bd7f717aeb99b898eae468
adabotcon <adabotcon@outlook.com>
2017-01-27 12:37:29 -0700

    Fixed error on object

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ var Uiname_BASE_URL = '';
 
 function getDataFromWordnik(wordType, callback){
 	var query = {
-		hasDictionaryDef = false,
-		includePartOfSpeech = wordType,
-		minCorpusCount = 0,
-		maxCorpusCount = -1,
-		minDictionaryCount = 1,
-		maxDictionaryCount = -1,
-		minLength = 4,
-		maxLength = -1,
-		api_key = '3c1825dd8857abc17e0060ff479098d6269c21519abe76465'
+		hasDictionaryDef: false,
+		includePartOfSpeech: wordType,
+		minCorpusCount: 0,
+		maxCorpusCount: -1,
+		minDictionaryCount: 1,
+		maxDictionaryCount: -1,
+		minLength: 4,
+		maxLength: -1,
+		api_key: '3c1825dd8857abc17e0060ff479098d6269c21519abe76465'
 	}
 
 	$.getJSON(Wordnik_BASE_URL, query, callback);
@@ -21,7 +21,12 @@ function getDataFromUiname(gender, region, callback){
 
 }
 
+function displayWordnikData(data){
+	console.log(data);
+}
+
 
 $(function(){
+	getDataFromWordnik('verb', displayWordnikData);
 	console.log('Hello World');
 })
